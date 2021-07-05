2f51b55c9688230a7c3f271b89f981a4065cdb7d
Rahul <rkc.98@outlook.com>
2021-07-05 10:55:53 +0530

    added footer

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -5,9 +5,10 @@ import Todo from "./Todo";
 const Todos=(props)=>{
     return <div className="container">
     <h1 className='text-center'>Todos List</h1>
-    {props.todo.map((todo)=>{
+    {props.todo.length===0?"no todos to display ": props.todo.map((todo)=>{
         return <Todo todosdisp={todo} onDelete={props.onDelete} />
     })}
+    
     
     </div>
 }
--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,8 +1,12 @@
 
-
+import './Footer.css'
 const Footer=()=>{
-    return <div>
-
-    </div>
+   
+    return <footer className='bg-dark text-light py-3 ftstyle' >
+        <p className='text-center'> 
+        try finishing your todos every day &copy;
+        </p>
+     
+    </footer>
 }
 export default Footer;
