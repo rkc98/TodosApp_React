0709c7d52a57232701485652e5ba3aba1c1f9a65
Rahul <rkc.98@outlook.com>
2021-07-05 12:22:08 +0530

    fix margin bug

--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -6,7 +6,10 @@ const Todos=(props)=>{
     return <div className="container">
     <h1 className='text-center'>Todos List</h1>
     {props.todo.length===0?"no todos to display ": props.todo.map((todo)=>{
-        return <Todo todosdisp={todo} onDelete={props.onDelete} />
+        return <div>
+            <Todo todosdisp={todo} onDelete={props.onDelete} />
+            <hr />
+            </div>
     })}
     
     
